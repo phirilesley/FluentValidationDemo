85af2f7578cad63216a7c2048ef09367c97f66f5
phirilesley <phirilesley@outlook.com>
2024-05-30 14:55:14 +0200

    c

---
+++ FluentValidationDemo/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidationDemo.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string ? Password { get; set; }
+}
---
+++ FluentValidationDemo/Validators/PersonValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FluentValidationDemo.Models;
+
+namespace FluentValidationDemo.Validators;
+
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().MaximumLength(50);
+        RuleFor(p => p.Email).NotEmpty().MaximumLength(50).EmailAddress();
+        RuleFor(p => p.Password).NotEmpty().Matches("^(?=.*\\d).*$").WithMessage("Must contain a numeric value");
+
+    }
+}
